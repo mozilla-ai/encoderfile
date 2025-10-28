e0bacb61ddddcdac4ce7d17eff52a26e302a67bd
Raz Besaleli <besaleli@mozilla.ai>
2025-10-28 13:22:45 -0400

    bones

---
+++ build.rs
@@ -0,0 +1,17 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tonic_prost_build::configure()
+        .protoc_arg("--experimental_allow_proto3_optional")
+        .build_server(true)
+        .out_dir("src/generated")
+        .compile_protos(&["proto/encoderfile.proto"], &["proto/encoderfile"])?;
+
+    let onnx_weights_path = std::env::var("MODEL_WEIGHTS_PATH").unwrap();
+    let tokenizer_path = std::env::var("TOKENIZER_PATH").unwrap();
+    let model_config_path = std::env::var("MODEL_CONFIG_PATH").unwrap();
+
+    println!("cargo:rustc-env=MODEL_WEIGHTS_PATH={}", &onnx_weights_path);
+    println!("cargo:rustc-env=TOKENIZER_PATH={}", &tokenizer_path);
+    println!("cargo:rustc-env=MODEL_CONFIG_PATH={}", &model_config_path);
+
+    Ok(())
+}
---
+++ src/assets.rs
@@ -0,0 +1,3 @@
+pub const MODEL_WEIGHTS: &[u8] = include_bytes!(env!("MODEL_WEIGHTS_PATH"));
+pub const TOKENIZER_JSON: &str = include_str!(env!("TOKENIZER_PATH"));
+pub const MODEL_CONFIG_JSON: &str = include_str!(env!("MODEL_CONFIG_PATH"));
