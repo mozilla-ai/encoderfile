d3e21387bf6b90f0cd1504fc2d70f64cb011aedd
Raz Besaleli <besaleli@mozilla.ai>
2025-10-29 16:49:03 -0400

    add sequence classification model

--- scripts/download_test_models.py
+++ scripts/download_test_models.py
@@ -1,8 +1,8 @@
 """Download models for testing."""
 
 import os
-from transformers import PreTrainedModel, AutoModel, AutoTokenizer
-from optimum.onnxruntime import ORTModel
+from transformers import AutoTokenizer
+from optimum.onnxruntime import ORTModel, ORTModelForSequenceClassification
 
 MODELS_DIR = "models/"
 
@@ -10,16 +10,12 @@ MODELS_DIR = "models/"
 def download_export_models(
     model_id: str,
     save_name: str,
-    transformers_cls: type[PreTrainedModel],
     ort_cls: type[ORTModel],
 ):
     save_dir = os.path.join(MODELS_DIR, save_name)
 
     tokenizer = AutoTokenizer.from_pretrained(model_id)
     tokenizer.save_pretrained(save_dir)
-
-    model = transformers_cls.from_pretrained(model_id)
-    model.save_pretrained(save_dir)
 
     model = ort_cls.from_pretrained(model_id, export=True)
     model.save_pretrained(save_dir)
@@ -28,5 +24,12 @@ def download_export_models(
 if __name__ == "__main__":
     # save embedding model
     download_export_models(
-        "microsoft/MiniLM-L12-H384-uncased", "embedding", AutoModel, ORTModel
+        "microsoft/MiniLM-L12-H384-uncased", "embedding", ORTModel
+    )
+
+    # save sequence classification model
+    download_export_models(
+        "tabularisai/multilingual-sentiment-analysis",
+        "sequence_classification",
+        ORTModelForSequenceClassification,
     )
