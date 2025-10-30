f3eaa5294c58bd11ad602e835bba3cea4698b5b9
Raz Besaleli <besaleli@mozilla.ai>
2025-10-30 12:29:25 -0400

    add banner

--- src/assets.rs
+++ src/assets.rs
@@ -2,3 +2,5 @@ pub const MODEL_TYPE_STR: &str = env!("MODEL_TYPE");
 pub const MODEL_WEIGHTS: &[u8] = include_bytes!(env!("MODEL_WEIGHTS_PATH"));
 pub const TOKENIZER_JSON: &str = include_str!(env!("TOKENIZER_PATH"));
 pub const MODEL_CONFIG_JSON: &str = include_str!(env!("MODEL_CONFIG_PATH"));
+
+pub const BANNER: &'static str = include_str!("../assets/banner.txt");
