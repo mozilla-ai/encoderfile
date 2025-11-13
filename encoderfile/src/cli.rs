ef9099910e7452b9067cba0e6afa39084e78fc28
Raz Besaleli <besaleli@mozilla.ai>
2025-11-13 15:52:58 -0500

    lint: fix clippy errors

--- encoderfile/src/cli.rs
+++ encoderfile/src/cli.rs
@@ -165,7 +165,7 @@ pub enum Format {
 impl Display for Format {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Format::Json => write!(f, "{}", "json")
+            Format::Json => write!(f, "json")
         }
     }
 }
