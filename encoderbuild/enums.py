62ef6a5ed8c6a1063a02543675fb895029b50770
Raz Besaleli <besaleli@mozilla.ai>
2025-11-18 22:25:02 +0400

    update

--- encoderbuild/enums.py
+++
@@ -1,7 +0,0 @@
-from enum import StrEnum
-
-
-class ModelType(StrEnum):
-    EMBEDDING = "embedding"
-    SEQUENCE_CLASSIFICATION = "sequence_classification"
-    TOKEN_CLASSIFICATION = "token_classification"
