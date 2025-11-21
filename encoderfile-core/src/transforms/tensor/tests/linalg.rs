46aa16c331529ca329441c86158dd42e74f38775
Raz Besaleli <besaleli@mozilla.ai>
2025-11-22 01:41:44 +0400

    update

--- encoderfile-core/src/transforms/tensor/tests/linalg.rs
+++ encoderfile-core/src/transforms/tensor/tests/linalg.rs
@@ -1,6 +1,5 @@
 use super::Tensor;
 use ndarray::{Array3, ArrayD, Axis};
-use ort::tensor::ArrayExtensions;
 
 #[test]
 fn test_transpose() {
@@ -8,18 +7,6 @@ fn test_transpose() {
     let transpose = arr.t().into_owned();
 
     assert_eq!(Tensor(arr).transpose().unwrap().0, transpose)
-}
-
-#[test]
-fn test_softmax() {
-    let arr: ArrayD<f32> = ndarray::array![[1.0, 2.0], [4.0, 5.0],].into_dyn();
-
-    let ts = Tensor(arr.clone());
-
-    assert_eq!(arr.softmax(Axis(0)), ts.softmax(1).unwrap().0);
-    assert_eq!(arr.softmax(Axis(1)), ts.softmax(2).unwrap().0);
-
-    assert_ne!(arr.softmax(Axis(1)), ts.softmax(1).unwrap().0);
 }
 
 #[test]
