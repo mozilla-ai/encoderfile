1e3ea10a1ba8b96acc4ba5663d93c6b1f4312276
Raz Besaleli <besaleli@mozilla.ai>
2025-12-03 15:23:38 +0400

    update

--- encoderfile/src/transforms/validation/embedding.rs
+++ encoderfile/src/transforms/validation/embedding.rs
@@ -2,14 +2,14 @@ use super::utils;
 use anyhow::{Context, Result, bail};
 use encoderfile_core::transforms::Transform;
 
-pub fn validate_transform(transform: Transform) -> Result<()> {
-    let batch_size = 32;
-    let seq_len = 128;
-    let hidden_dim = 384;
+const BATCH_SIZE: usize = 32;
+const SEQ_LEN: usize = 128;
+const HIDDEN_DIM: usize = 384;
 
+pub fn validate_transform(transform: Transform) -> Result<()> {
     // create dummy hidden states with shape [batch_size, seq_len, hidden_dim]
     let dummy_hidden_states =
-        utils::random_tensor(&[batch_size, seq_len, hidden_dim], (-1.0, 1.0))?;
+        utils::random_tensor(&[BATCH_SIZE, SEQ_LEN, HIDDEN_DIM], (-1.0, 1.0))?;
 
     let res = transform.postprocess(dummy_hidden_states)
         .with_context(|| "Failed to run postprocessing on dummy embeddings (randomly generated in range -1.0..1.0) of shape [32, 128, 384].")?;
@@ -23,11 +23,11 @@ pub fn validate_transform(transform: Transform) -> Result<()> {
     }
 
     // result must have same batch_size and seq_len
-    if res.shape()[0] != batch_size || res.shape()[1] != seq_len {
+    if res.shape()[0] != BATCH_SIZE || res.shape()[1] != SEQ_LEN {
         bail!(
             "Transform must preserve batch and seq dims [{} {}, *]. Got shape {:?}",
-            batch_size,
-            seq_len,
+            BATCH_SIZE,
+            SEQ_LEN,
             res.shape()
         );
     }
