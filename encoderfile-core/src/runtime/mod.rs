41c0becda97f6a7061095eab8850c2afb14b6b94
Raz Besaleli <besaleli@mozilla.ai>
2025-12-11 14:46:20 +0400

    refactor config to model_config

--- encoderfile-core/src/runtime/mod.rs
+++ encoderfile-core/src/runtime/mod.rs
@@ -1,9 +1,9 @@
-mod config;
+mod model_config;
 mod model;
 mod state;
 mod tokenizer;
 
-pub use config::{get_model_config, get_model_type};
+pub use model_config::{get_model_config, get_model_type};
 pub use model::{Model, get_model};
 pub use state::{AppState, InferenceState};
 pub use tokenizer::{encode_text, get_tokenizer, get_tokenizer_from_string};
