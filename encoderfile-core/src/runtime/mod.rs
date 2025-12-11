568177c1cfc9148caec1f00224305e0a02e0313d
Raz Besaleli <besaleli@mozilla.ai>
2025-12-11 15:20:28 +0400

    remove old model_type

--- encoderfile-core/src/runtime/mod.rs
+++ encoderfile-core/src/runtime/mod.rs
@@ -6,6 +6,6 @@ mod tokenizer;
 
 pub use config::get_config;
 pub use model::{Model, get_model};
-pub use model_config::{get_model_config, get_model_type};
+pub use model_config::get_model_config;
 pub use state::{AppState, InferenceState};
 pub use tokenizer::{encode_text, get_tokenizer, get_tokenizer_from_string};
--- encoderfile-core/src/runtime/model_config.rs
+++ encoderfile-core/src/runtime/model_config.rs
@@ -1,7 +1,6 @@
-use crate::common::{ModelConfig, ModelType};
+use crate::common::ModelConfig;
 use std::sync::{Arc, OnceLock};
 
-static MODEL_TYPE: OnceLock<ModelType> = OnceLock::new();
 static MODEL_CONFIG: OnceLock<Arc<ModelConfig>> = OnceLock::new();
 
 pub fn get_model_config(config_str: &str) -> Arc<ModelConfig> {
@@ -9,18 +8,6 @@ pub fn get_model_config(config_str: &str) -> Arc<ModelConfig> {
         .get_or_init(|| match serde_json::from_str::<ModelConfig>(config_str) {
             Ok(c) => Arc::new(c),
             Err(e) => panic!("FATAL: Error loading model config: {e:?}"),
-        })
-        .clone()
-}
-
-pub fn get_model_type(model_type: &str) -> ModelType {
-    MODEL_TYPE
-        .get_or_init(|| match model_type {
-            "embedding" => ModelType::Embedding,
-            "sequence_classification" => ModelType::SequenceClassification,
-            "token_classification" => ModelType::TokenClassification,
-            "sentence_embedding" => ModelType::SentenceEmbedding,
-            other => panic!("Invalid model type: {other}"),
         })
         .clone()
 }
