904be63dcdf57586d2b4b0ed036278387bbbea72
Raz Besaleli <besaleli@mozilla.ai>
2026-01-07 16:01:58 +0400

    remove feature flags for generated protobufs

--- encoderfile-core/src/generated/mod.rs
+++ encoderfile-core/src/generated/mod.rs
@@ -1,14 +1,7 @@
-#[cfg(feature = "transport")]
 pub mod embedding;
-#[cfg(feature = "transport")]
 pub mod sentence_embedding;
-#[cfg(feature = "transport")]
 pub mod sequence_classification;
-#[cfg(feature = "transport")]
 pub mod token;
-#[cfg(feature = "transport")]
 pub mod token_classification;
-
 pub mod metadata;
-
 pub mod manifest;
