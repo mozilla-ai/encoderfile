724d05953463c92d56ac8c87d5b84d9d7d25fd1a
Raz Besaleli <besaleli@mozilla.ai>
2026-01-07 15:59:47 +0400

    update deps

--- encoderfile-core/src/generated/mod.rs
+++ encoderfile-core/src/generated/mod.rs
@@ -1,12 +1,13 @@
+#[cfg(feature = "transport")]
 pub mod embedding;
-
-pub mod sequence_classification;
-
-pub mod token_classification;
-
+#[cfg(feature = "transport")]
 pub mod sentence_embedding;
-
+#[cfg(feature = "transport")]
+pub mod sequence_classification;
+#[cfg(feature = "transport")]
 pub mod token;
+#[cfg(feature = "transport")]
+pub mod token_classification;
 
 pub mod metadata;
 
