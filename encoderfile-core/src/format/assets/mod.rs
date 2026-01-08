9396d64da90b7368b6f44bacc360cdff952bb6fb
Raz Besaleli <besaleli@mozilla.ai>
2026-01-08 14:09:04 +0400

    refactor

--- encoderfile-core/src/format/assets/mod.rs
+++ encoderfile-core/src/format/assets/mod.rs
@@ -1,29 +1,11 @@
 use anyhow::Result;
 
 mod kind;
-mod planned_asset;
+mod plan;
 mod source;
 
-pub use self::{kind::AssetKind, planned_asset::PlannedAsset, source::AssetSource};
-
-pub struct AssetPlan<'a> {
-    assets: Vec<PlannedAsset<'a>>,
-}
-
-impl<'a> AssetPlan<'a> {
-    pub fn new(mut assets: Vec<PlannedAsset<'a>>) -> Result<Self> {
-        assets.sort_by_key(|a| a.kind.clone());
-
-        for w in assets.windows(2) {
-            if w[0].kind == w[1].kind {
-                anyhow::bail!("duplicate asset kind {:?}", w[0].kind);
-            }
-        }
-
-        Ok(Self { assets })
-    }
-
-    pub fn assets(&self) -> &[PlannedAsset<'a>] {
-        &self.assets
-    }
-}
+pub use self::{
+    kind::AssetKind,
+    plan::{AssetPlan, PlannedAsset},
+    source::AssetSource,
+};
--- encoderfile-core/src/format/assets/plan.rs
+++ encoderfile-core/src/format/assets/plan.rs
@@ -2,6 +2,29 @@ use super::{kind::AssetKind, source::AssetSource};
 use anyhow::Result;
 
 #[derive(Debug)]
+pub struct AssetPlan<'a> {
+    assets: Vec<PlannedAsset<'a>>,
+}
+
+impl<'a> AssetPlan<'a> {
+    pub fn new(mut assets: Vec<PlannedAsset<'a>>) -> Result<Self> {
+        assets.sort_by_key(|a| a.kind.clone());
+
+        for w in assets.windows(2) {
+            if w[0].kind == w[1].kind {
+                anyhow::bail!("duplicate asset kind {:?}", w[0].kind);
+            }
+        }
+
+        Ok(Self { assets })
+    }
+
+    pub fn assets(&self) -> &[PlannedAsset<'a>] {
+        &self.assets
+    }
+}
+
+#[derive(Debug)]
 pub struct PlannedAsset<'a> {
     pub kind: AssetKind,
     pub length: u64,
