749d9c5676e82905d6935868e1b62733cfe79a5a
Raz Besaleli <besaleli@mozilla.ai>
2026-01-08 20:54:05 +0400

    remove old appstate loader

--- encoderfile-core/src/format/mod.rs
+++ encoderfile-core/src/format/mod.rs
@@ -2,3 +2,4 @@ pub mod assets;
 pub mod codec;
 pub mod container;
 pub mod footer;
+pub mod loader;
--- encoderfile-core/src/runtime/mod.rs
+++ encoderfile-core/src/runtime/mod.rs
@@ -7,5 +7,5 @@ mod tokenizer;
 pub use config::get_config;
 pub use model::{Model, get_model};
 pub use model_config::get_model_config;
-pub use state::{AppState, AppStateLoader, InferenceState};
+pub use state::{AppState, InferenceState};
 pub use tokenizer::{TokenizerService, get_tokenizer, get_tokenizer_from_string};
