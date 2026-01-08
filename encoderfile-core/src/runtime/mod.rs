ef3ddeb5e1b320cc66719ac6288b6421f1917893
Raz Besaleli <besaleli@mozilla.ai>
2026-01-08 20:55:34 +0400

    refactor

--- encoderfile-core/src/runtime/mod.rs
+++ encoderfile-core/src/runtime/mod.rs
@@ -1,13 +1,13 @@
 mod config;
+mod loader;
 mod model;
 mod model_config;
 mod state;
 mod tokenizer;
-mod loader;
 
 pub use config::get_config;
+pub use loader::EncoderfileLoader;
 pub use model::{Model, get_model};
 pub use model_config::get_model_config;
 pub use state::{AppState, InferenceState};
 pub use tokenizer::{TokenizerService, get_tokenizer, get_tokenizer_from_string};
-pub use loader::EncoderfileLoader;
