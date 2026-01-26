cbf1d6e3cb79835a6ba09a8fc09a4af55de21e49
Raz Besaleli <besaleli@mozilla.ai>
2026-01-26 12:58:05 +0100

    metadata trait

--- encoderfile/src/services/model_metadata.rs
+++ encoderfile/src/services/model_metadata.rs
@@ -1,18 +1,36 @@
+use std::collections::HashMap;
+
 use crate::{
-    common::{GetModelMetadataResponse, model_type::ModelTypeSpec},
+    common::{GetModelMetadataResponse, ModelType, model_type::ModelTypeSpec},
     runtime::AppState,
 };
 
 pub trait Metadata {
-    fn metadata(&self) -> GetModelMetadataResponse;
+    fn metadata(&self) -> GetModelMetadataResponse {
+        GetModelMetadataResponse {
+            model_id: self.model_id(),
+            model_type: self.model_type(),
+            id2label: self.id2label(),
+        }
+    }
+
+    fn model_id(&self) -> String;
+
+    fn model_type(&self) -> ModelType;
+
+    fn id2label(&self) -> Option<HashMap<u32, String>>;
 }
 
 impl<T: ModelTypeSpec> Metadata for AppState<T> {
-    fn metadata(&self) -> GetModelMetadataResponse {
-        GetModelMetadataResponse {
-            model_id: self.config.name.clone(),
-            model_type: T::enum_val(),
-            id2label: self.model_config.id2label.clone(),
-        }
+    fn model_id(&self) -> String {
+        self.config.name.clone()
+    }
+
+    fn model_type(&self) -> ModelType {
+        T::enum_val()
+    }
+
+    fn id2label(&self) -> Option<HashMap<u32, String>> {
+        self.model_config.id2label.clone()
     }
 }
