f34082c92777fd03e238a1f130ed98fcc9559f97
Javier Torres <javier@mozilla.ai>
2026-03-11 15:55:17 +0100

    Rename and unde-del core lib

--- encoderfile-py/python/encoderfile/__init__.py
+++ encoderfile-py/python/encoderfile/__init__.py
@@ -1,9 +1,7 @@
 # type: ignore
-from ._encoderfile_rust import *  # noqa: F403
+from ._core import *  # noqa: F403
 from .enums import *  # noqa: F403
 
-__doc__ = _encoderfile_rust.__doc__  # noqa: F405
-if hasattr(_encoderfile_rust, "__all__"):  # noqa: F405
-    __all__ = _encoderfile_rust.__all__  # noqa: F405
-
-del _encoderfile_rust  # noqa: F821
+__doc__ = _core.__doc__  # noqa: F405
+if hasattr(_core, "__all__"):  # noqa: F405
+    __all__ = _core.__all__  # noqa: F405
