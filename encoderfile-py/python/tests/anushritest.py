850555be6dbcbfe6065bb0fa8b1a74dae46e2b44
angpt <anushrigupta@gmail.com>
2026-04-03 12:38:56 -0500

    Delete anushritest.py

--- encoderfile-py/python/tests/anushritest.py
+++
@@ -1,9 +0,0 @@
-
-from encoderfile import read_metadata
-
-info = read_metadata("encoderfile-py/python/tests/sentiment-analyzer.encoderfile")
-
-print(info.encoderfile_config.name)        # "sentiment-analyzer"
-print(info.encoderfile_config.model_type)  # "sequence_classification"
-print(info.encoderfile_config.version)     # "1.0.0"
-print(info.model_config.id2label)          # {0: "NEGATIVE", 1: "POSITIVE"}
